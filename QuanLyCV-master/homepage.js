51555a4e94a487be4a2500e8f624d8358a43073c
Just625 <60479882+Just625@users.noreply.github.com>
2020-06-04 22:14:11 +0700

    Add files via upload

--- QuanLyCV-master/homepage.js
+++ QuanLyCV-master/homepage.js
@@ -5,7 +5,7 @@ const addButton = document.getElementById('addButton')
 //Current date
 const currentDate = document.getElementById('currentDate')
 let today  = new Date()
-console.log(today)
+// console.log(today)
 let weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 let date =  weekday[today.getDay()] + ',' + today.getDate() + '/' +  (today.getMonth()+1) +'/'+ today.getFullYear()
 currentDate.insertAdjacentHTML('beforeend', ` ${date}`)
@@ -15,11 +15,29 @@ function render(){
     ulElement.innerHTML = ''
     LIST.forEach(function(item,index){
         innerHTML =  
-        `<li>${item}</li>
-        <button class = 'delButton' id=${index}>Erase it</button>`
+        `<li>
+        <i class="far fa-circle"></i>
+        <span >${item}</span>
+        <i id=${index} class="fa fa-trash"></i>
+        </li>`
         ulElement.insertAdjacentHTML('beforeend', innerHTML)
     })
 }
+
+// const checkButton = document.getElementsByClassName('far fa-circle')
+// function check(){
+//     for ( let i = 0; i < checkButton.length; i ++){
+//         console.log(LIST)
+//         checkButton[i].addEventListener('click', function(e){
+//             const index = e.target.id 
+//             LIST[i].classList.toggle("done")
+//             render()
+//             remove()
+//             check()
+//         })
+//     }
+// }
+
 const addText = document.getElementById('addText')
 addText.addEventListener('keyup', function(event){
         if (event.keyCode === 13){
@@ -29,16 +47,19 @@ addText.addEventListener('keyup', function(event){
 
 addButton.addEventListener('click',function(){
     const newItem = addText.value
-    LIST.push(newItem)
-    addText.value = ''
-    render()
-    remove()
+    if (newItem !== ''){
+        LIST.push(newItem)
+        addText.value = ''
+        render()
+        remove()
+    }else{
+        alert('You must write something in')
+    }
    
 })
 
 //Erase item from list
-const delButton = document.getElementsByClassName('delButton')
-// console.log(delButton)
+const delButton = document.getElementsByClassName('fa fa-trash')
 function remove(){
     for ( let i = 0; i < delButton.length; i ++){
         console.log(LIST)
