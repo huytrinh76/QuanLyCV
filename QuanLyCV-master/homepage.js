a8601d7b04e8bb7222ed97eca44b9bd7acc5cb0c
Just625 <60479882+Just625@users.noreply.github.com>
2020-06-07 07:04:29 +0700

    Add checked circle when user done some tasks

--- QuanLyCV-master/homepage.js
+++ QuanLyCV-master/homepage.js
@@ -5,7 +5,6 @@ const addButton = document.getElementById('addButton')
 //Current date
 const currentDate = document.getElementById('currentDate')
 let today  = new Date()
-// console.log(today)
 let weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 let date =  weekday[today.getDay()] + ',' + today.getDate() + '/' +  (today.getMonth()+1) +'/'+ today.getFullYear()
 currentDate.insertAdjacentHTML('beforeend', ` ${date}`)
@@ -16,28 +15,30 @@ function render(){
     LIST.forEach(function(item,index){
         innerHTML =  
         `<li>
-        <i class="far fa-circle"></i>
-        <span >${item}</span>
-        <i id=${index} class="fa fa-trash"></i>
+        <i class="far fa-circle" id='circle${index}'></i>
+        <span id =${index}>${item}</span>
+        <i  class="fa fa-trash" ></i>
         </li>`
         ulElement.insertAdjacentHTML('beforeend', innerHTML)
     })
 }
 
-// const checkButton = document.getElementsByClassName('far fa-circle')
-// function check(){
-//     for ( let i = 0; i < checkButton.length; i ++){
-//         console.log(LIST)
-//         checkButton[i].addEventListener('click', function(e){
-//             const index = e.target.id 
-//             LIST[i].classList.toggle("done")
-//             render()
-//             remove()
-//             check()
-//         })
-//     }
-// }
+// Add check button
+const checkButton = document.getElementsByClassName('far fa-circle')
+function check(){
+    for ( let i = 0; i < checkButton.length; i ++){
+        checkButton[i].addEventListener('click', function(){           
+            const checkCircle = document.getElementById(`circle${i}`)
+            checkCircle.classList.toggle('fa-circle')
+            checkCircle.classList.toggle('fa-dot-circle')    
+            const checkElement = document.getElementById(`${i}`)
+            checkElement.classList.toggle("done")
+        }
+        )}
+}
 
+
+//Add add items button
 const addText = document.getElementById('addText')
 addText.addEventListener('keyup', function(event){
         if (event.keyCode === 13){
@@ -52,6 +53,7 @@ addButton.addEventListener('click',function(){
         addText.value = ''
         render()
         remove()
+        check()
     }else{
         alert('You must write something in')
     }
@@ -62,12 +64,13 @@ addButton.addEventListener('click',function(){
 const delButton = document.getElementsByClassName('fa fa-trash')
 function remove(){
     for ( let i = 0; i < delButton.length; i ++){
-        console.log(LIST)
+        // console.log(LIST)
         delButton[i].addEventListener('click', function(e){
             const index = e.target.id 
             LIST.splice(index, 1)
             render()
             remove()
+            check()
         })
     }
 }
